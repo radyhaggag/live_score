1d06ac70c39abd65b820c21ba1d328bdcb4f7c36
radyhaggag <radyhaggag948@gmail.com>
2022-08-25 22:16:10 +0200

    problem in button don't show in real phone only !

--- lib/src/features/soccer/presentation/widgets/block_dialog.dart
+++ lib/src/features/soccer/presentation/widgets/block_dialog.dart
@@ -5,40 +5,52 @@ import '../../../../core/utils/app_colors.dart';
 import '../../../../core/utils/app_size.dart';
 import '../../../../core/utils/app_strings.dart';
 
+class BlockAlert extends StatelessWidget {
+  final String message;
+
+  const BlockAlert({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Icon(
+          Icons.error_outline,
+          color: AppColors.red,
+          size: AppSize.s90,
+        ),
+        const SizedBox(height: AppSize.s10),
+        Text(
+          message,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        const SizedBox(height: AppSize.s10),
+        SizedBox(
+          width: context.width / 2,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: AppColors.blue,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pushReplacementNamed(Routes.soccerLayout);
+            },
+            child: const Text(AppStrings.reload),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 void buildBlockAlert({required BuildContext context, required String message}) {
   showDialog(
     context: context,
     barrierDismissible: false,
     builder: (_) => AlertDialog(
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Icon(
-            Icons.error_outline,
-            color: AppColors.red,
-            size: AppSize.s90,
-          ),
-          const SizedBox(height: AppSize.s10),
-          Text(
-            message,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          const SizedBox(height: AppSize.s10),
-          if (message != AppStrings.reachedLimits)
-            SizedBox(
-              width: context.width / 2,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context)
-                      .pushReplacementNamed(Routes.soccerLayout);
-                },
-                child: const Text(AppStrings.reload),
-              ),
-            ),
-        ],
-      ),
+      content: BlockAlert(message: message),
     ),
   );
 }
