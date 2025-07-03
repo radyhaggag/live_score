6f241f0421f6b21432c37f884d365dc44c0d9697
Rady Haggag <radyhaggag50@gmail.com>
2025-07-03 19:00:59 +0300

    chore: update compileSdk and targetSdk to 35, increment version to 1.0.1+6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.radyhaggag.livescore"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
@@ -35,7 +35,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
